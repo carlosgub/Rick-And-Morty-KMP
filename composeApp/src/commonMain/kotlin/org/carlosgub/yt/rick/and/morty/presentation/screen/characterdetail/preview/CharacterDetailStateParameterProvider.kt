36bd406042e914a36ddb43a3ab08cf0972e53218
carlosgub <carlosgabrielugazburga@gmail.com>
2026-01-26 23:31:53 -0500

    Parte 11

---
+++ composeApp/src/commonMain/kotlin/org/carlosgub/yt/rick/and/morty/presentation/screen/characterdetail/preview/CharacterDetailStateParameterProvider.kt
@@ -0,0 +1,27 @@
+package org.carlosgub.yt.rick.and.morty.presentation.screen.characterdetail.preview
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import org.carlosgub.yt.rick.and.morty.domain.model.Character
+import org.carlosgub.yt.rick.and.morty.presentation.viewmodel.characterdetail.CharacterDetailState
+
+class CharacterDetailStateParameterProvider: PreviewParameterProvider<CharacterDetailState> {
+    override val values: Sequence<CharacterDetailState> = sequenceOf(
+        CharacterDetailState(
+            isLoading = true,
+            character = null
+        ),
+        CharacterDetailState(
+            isLoading = false,
+            character = Character(
+                id = 1,
+                name = "Rick Sanchez",
+                status = "Alive",
+                species = "Human",
+                type = "",
+                gender = "Male",
+                location = "Citadel of Ricks",
+                image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            )
+        )
+    )
+}
